be6ad9bc2d44fb14673f552ef4231cf27f7b69ab
Noah Till <ntill@gmx.de>
2020-12-03 13:35:03 +0100

    adding item

---
+++ public/yukisora/devs/item/main.js
@@ -0,0 +1,105 @@
+var error = "";
+var token;
+
+async function init() {
+    token = getCookie("auth");
+    await fillItem();
+    var errort = document.getElementById('error');
+    errort.innerHTML = error;
+}
+
+async function fillItem() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth-token': token
+        }
+    };
+    const response = await fetch('/api/yuki/items', options);
+    const json = await response.json();
+    if (json.status == 200) {
+        for (var i = 0; i < json.data.length; i++) {
+            createItem(json.data[i].attackName, json.data[i]._id);
+        }
+    } else {
+        error += json.message;
+    }
+}
+
+function createItem(name, idd) {
+    var id = document.createElement('p');
+    id.setAttribute("hidden", true);
+    id.innerHTML = idd;
+    var list = document.getElementById('cooks');
+    var di = document.createElement('div');
+    var lable = document.createElement('label');
+    lable.innerHTML = name;
+    var inp = document.createElement('input');
+    inp.setAttribute("type", "checkbox");
+    di.appendChild(inp);
+    di.appendChild(lable);
+    di.appendChild(id);
+    list.appendChild(di);
+}
+
+function getCheckedItem() {
+    var list = document.getElementById('list');
+    var divs = list.getElementsByTagName('div');
+    var ids = [];
+    for (var i = 0; i < divs.length; i += 1) {
+        var id = divs[i].getElementsByTagName('p')[0].innerHTML;
+        if (divs[i].getElementsByTagName('input')[0].checked) {
+            ids.push(id);
+        }
+    }
+    if (ids.length > 1) {
+        var errort = document.getElementById('error');
+        errort.innerHTML = "Please select only one item!";
+        return undefined;
+    } else
+        return ids;
+}
+
+async function submit() {
+    const item = getCheckedItem();
+    const name = document.getElementById("name").value;
+    const image = document.getElementById("imageUrl").value;
+    const cookable = document.getElementById("cookable").checked;
+    const des = document.getElementById("description").value;
+    var e = document.getElementById("rarity");
+    const select = e.options[e.selectedIndex].text;
+
+    if (!item && cookable) {
+        var errort = document.getElementById('error');
+        errort.innerHTML = "Please select one item!";
+        return;
+    }
+
+    const json = {
+        itemName: name,
+        itemRarity: select,
+        itemImageURL: image,
+        itemDescription: des,
+        isitemCookable: cookable,
+        cooksInto: item
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth-token': token
+        },
+        body: JSON.stringify(json)
+    };
+
+    const response = await fetch('/api/yuki/monsters', options);
+    var errort = document.getElementById('error');
+    if (response)
+        if (response.status == 200)
+            errort.innerHTML = "Succesfully added monster!";
+        else
+            errort.innerHTML = "An error occured! " + response.body;
+    else
+        errort.innerHTML = "An error occured! Fetching wasnt possible";
+}
--- router/yuki.js
+++ router/yuki.js
@@ -4,35 +4,38 @@ const vAdmin = require("../middleware/verifyAdminAcces");
 const fetch = require('node-fetch');
 
 router.get("/monsters", verify, async(req, res) => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'api-token': process.env.YUKIDB_API_TOKEN
-        }
-    };
-
-    fetch('http://127.0.0.1:5004/api/yuki/monster', options).then(res => res.json()).then(json => {
-        res.status(200).json(json);
-    });
+    makeGet(res, "monster");
 });
 
 router.post("/monsters", verify, async(req, res) => {
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'api-token': process.env.YUKIDB_API_TOKEN
-        },
-        body: JSON.stringify(req.body)
-    };
-
-    fetch('http://127.0.0.1:5004/api/yuki/monster', options).then(res => res.json()).then(json => {
-        res.status(200).json(json);
-    });
+    makePost(req, res, "monster");
 });
 
 router.get("/attacks", verify, async(req, res) => {
+    makeGet(res, "attack");
+});
+
+router.post("/attacks", verify, async(req, res) => {
+    makePost(req, res, "attack");
+});
+
+router.get("/items", verify, async(req, res) => {
+    makeGet(res, "item");
+});
+
+router.post("/items", verify, async(req, res) => {
+    makePost(req, res, "item");
+});
+
+router.get("/jobs", verify, async(req, res) => {
+    makeGet(res, "job");
+});
+
+router.post("/jobs", verify, async(req, res) => {
+    makePost(req, res, "job");
+});
+
+function makeGet(res, api) {
     const options = {
         method: 'GET',
         headers: {
@@ -41,12 +44,12 @@ router.get("/attacks", verify, async(req, res) => {
         }
     };
 
-    fetch('http://127.0.0.1:5004/api/yuki/attack', options).then(res => res.json()).then(json => {
+    fetch('http://127.0.0.1:5004/api/yuki/' + api, options).then(res => res.json()).then(json => {
         res.status(200).json(json);
     });
-});
+}
 
-router.post("/attacks", verify, async(req, res) => {
+function makePost(req, res, api) {
     const options = {
         method: 'POST',
         headers: {
@@ -56,9 +59,9 @@ router.post("/attacks", verify, async(req, res) => {
         body: JSON.stringify(req.body)
     };
 
-    fetch('http://127.0.0.1:5004/api/yuki/attack', options).then(res => res.json()).then(json => {
+    fetch('http://127.0.0.1:5004/api/yuki/' + api, options).then(res => res.json()).then(json => {
         res.status(200).json(json);
     });
-});
+}
 
 module.exports = router;
