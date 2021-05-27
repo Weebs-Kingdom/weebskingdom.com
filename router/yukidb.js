9c6a937ee7fd18a4ef76da2f769a3330f36211ce
Noah Till <NTill@gmx.de>
2021-05-27 13:25:42 +0200

    info.css

--- router/yukidb.js
+++ router/yukidb.js
@@ -6,17 +6,17 @@ const User = require("../models/user/User");
 const nodeHtmlToImage = require('node-html-to-image')
 const ejs = require("ejs")
 
-router.post("/getLevelInfo", async (req, res) => {
+router.post("/getLevelInfo", verifyApi, async (req, res) => {
 
-    const data = await ejs.renderFile("./views/userprofile.ejs", {name: "Test name", level: "100", xp: "100"});
+    const data = await ejs.renderFile("./views/userprofile.ejs", {name: "Test name", level: "100", xp: "100", weboos: "100", jlevel: "100", jxp: "50", energy: "50", job: "Worker", jobpos: "Trainee"});
 
     const url = makeToken(10) + "-img.png";
 
     nodeHtmlToImage({
-        output: "./public/img/info/" + url,
+        output: "./" + url,
         html: data
     })
-        .then(() => res.status(200).json({data: "https://weebskingdom.com/img/info/" + url,message: "complete"}));
+        .then(() => res.status(200).json({data: "https://weebskingdom.com/img/info/" + url, message: "complete"}));
 });
 
 function makeToken(length) {
